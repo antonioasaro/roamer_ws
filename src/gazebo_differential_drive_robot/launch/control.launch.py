2041132f6b32600ac8f3bc8941f7b8912226a57f
antonioasaro <antonio_asaro@hotmail.com>
2026-08-23 20:15:59 +0000

    Added ros2_control

---
+++ src/gazebo_differential_drive_robot/launch/control.launch.py
@@ -0,0 +1,81 @@
+import os
+
+from sympy import true
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, RegisterEventHandler, DeclareLaunchArgument
+from launch.event_handlers import OnProcessExit
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration
+import xacro
+
+def generate_launch_description():
+    pkg_share = get_package_share_directory('gazebo_differential_drive_robot')
+    
+    # 1. Parse Xacro
+    xacro_file = os.path.join(pkg_share, 'urdf', 'robot.xacro')
+    robot_description_raw = xacro.process_file(xacro_file).toxml()
+    
+    world_arg = DeclareLaunchArgument(
+        'world',
+        default_value='empty.sdf',
+        description='Specify the world file for Gazebo (e.g., empty.sdf)'
+    )
+    world_file = LaunchConfiguration('world')
+    
+    # 2. Robot State Publisher
+    node_robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[{'robot_description': robot_description_raw, 'use_sim_time': True}]
+    )
+
+    # 3. Gazebo Sim Launch
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')]),
+            launch_arguments={
+                'gz_args': [f'-r -v 4 ', world_file],
+                'on_exit_shutdown': 'true'
+            }.items()
+        #launch_arguments={'gz_args': '-r empty.sdf'}.items()
+    )
+
+    # 4. Spawn Entity
+    gz_spawn_entity = Node(
+        package='ros_gz_sim',
+        executable='create',
+        arguments=['-topic', 'robot_description', '-name', 'differential_drive_robot'],
+        output='screen'
+    )
+
+    # 5. Controller Spawners
+    joint_broad_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=['joint_state_broadcaster']
+    )
+
+    diff_drive_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=['diff_drive_controller']
+    )
+
+    # Delay execution to prevent race conditions during Gazebo initial load
+    delay_diff_drive = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=joint_broad_spawner,
+            on_exit=[diff_drive_spawner],
+        )
+    )
+
+    return LaunchDescription([
+        node_robot_state_publisher,
+        gazebo,
+        gz_spawn_entity,
+        joint_broad_spawner,
+        delay_diff_drive
+    ])
