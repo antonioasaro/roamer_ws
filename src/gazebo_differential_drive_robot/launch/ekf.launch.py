9a70d8eb2efcfd29c29103f1f5ce146a0b05b01c
antonioasaro <antonio_asaro@hotmail.com>
2026-09-04 20:40:59 +0000

    Try using slambot files

--- src/gazebo_differential_drive_robot/launch/ekf.launch.py
+++ src/gazebo_differential_drive_robot/launch/ekf.launch.py
@@ -24,10 +24,7 @@ def generate_launch_description():
         parameters=[
             '/home/ubuntu/roamer_ws/src/gazebo_differential_drive_robot/config/ekf_params.yaml',
             {'use_sime_time': use_sim_time}
-            ],
-        remappings=[('/odometry/filtered', '/odom'),
-                    ('/imu/data', '/imu_state_broadcaster/imu')
-                    ]
+            ]
     )
     
     return LaunchDescription([
