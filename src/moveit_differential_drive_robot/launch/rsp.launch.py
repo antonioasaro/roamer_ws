57e88f8e61a93949fc7d683ae2816ebebcaf4c36
antonioasaro <antonio.asaro@hotmail.com>
2026-09-08 13:31:47 +0000

    Re-run moveit assistant

---
+++ src/moveit_differential_drive_robot/launch/rsp.launch.py
@@ -0,0 +1,7 @@
+from moveit_configs_utils import MoveItConfigsBuilder
+from moveit_configs_utils.launches import generate_rsp_launch
+
+
+def generate_launch_description():
+    moveit_config = MoveItConfigsBuilder("differential_drive_robot", package_name="moveit_differential_drive_robot").to_moveit_configs()
+    return generate_rsp_launch(moveit_config)
