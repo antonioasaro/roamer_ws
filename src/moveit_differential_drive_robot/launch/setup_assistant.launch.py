e1242c2967520a520423db094d501e6b50b41a18
antonioasaro <antonio_asaro@hotmail.com>
2026-09-09 11:40:19 -0400

    Removed space from moveit project name in py files

--- src/moveit_differential_drive_robot/launch/setup_assistant.launch.py
+++ src/moveit_differential_drive_robot/launch/setup_assistant.launch.py
@@ -3,5 +3,5 @@ from moveit_configs_utils.launches import generate_setup_assistant_launch
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("differential_drive_robot", package_name="moveit_differential_drive_robot ").to_moveit_configs()
+    moveit_config = MoveItConfigsBuilder("differential_drive_robot", package_name="moveit_differential_drive_robot").to_moveit_configs()
     return generate_setup_assistant_launch(moveit_config)
--- src/moveit_differential_drive_robot/launch/warehouse_db.launch.py
+++ src/moveit_differential_drive_robot/launch/warehouse_db.launch.py
@@ -3,5 +3,5 @@ from moveit_configs_utils.launches import generate_warehouse_db_launch
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("differential_drive_robot", package_name="moveit_differential_drive_robot ").to_moveit_configs()
+    moveit_config = MoveItConfigsBuilder("differential_drive_robot", package_name="moveit_differential_drive_robot").to_moveit_configs()
     return generate_warehouse_db_launch(moveit_config)
