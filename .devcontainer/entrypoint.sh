e1699bf6dca4c0026aaf5710089f5b7d70317557
antonioasaro <antonio_asaro@hotmail.com>
2026-09-10 00:54:52 +0000

    Dropped moveit virtual_joint

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -32,12 +32,6 @@ if [ -n "${WORKSPACE}" ]; then
     else
         echo "Warning: install/setup.bash not found even after build."
     fi
-
-    if [ -f "${WORKSPACE}/howto/howto.aliases" ]; then
-        source "${WORKSPACE}/howto/howto.aliases"
-    else
-        echo "Warning: howto.aliases not found."
-    fi
 else
     echo "Warning: WORKSPACE variable not set."
 fi
